8b7e16a33b6347dbcbde6ce8293dadf7ee73482d
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-10-03 15:05:06 -0700

    Sixth Company logo welcome splash screen implemented

--- app/src/main/java/com/flowz/sixtjobapp2/MainActivity.kt
+++ app/src/main/java/com/flowz/sixtjobapp2/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.SixtJobApp2)
         setContentView(R.layout.activity_main)
 
         navController = findNavController( R.id.nav_host_fragment)
